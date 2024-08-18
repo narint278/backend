bf65437d74ddd229b11bf00e0b6cdd667c063bab
rinrin888 <narint278@gmail.com>
2024-08-18 10:40:23 +0800

    แก้ไขเส้นทางไฟล์ static

--- index.js
+++ index.js
@@ -181,8 +181,9 @@ app.use((err, req, res, next) => {
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
 });
+
 
 app.listen(port, () => {
   connect();
